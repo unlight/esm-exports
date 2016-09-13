571bbd98a6aeec2cecc293c29236a30094fe42b3
Roman Vasilev <s91630277@gmail.com>
2016-09-13 16:42:45 +0300

    Removed .npmignore, because we will use github

---
+++ lib/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
+export default function main(name: string, {baseDir}?: {
+    [k: string]: any;
+}): Promise<Array<any>>;
---
+++ lib/parse.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
+export default function parse(file: string, options?: any): any;
