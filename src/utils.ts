a6fc7976b9a46f76e89597d1ca391f627a6111ee
iamthes <s91630277@gmail.com>
2017-03-25 23:34:35 +0400

    get rid of recursive-readdir

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import * as Path from 'path';
 import { Entry } from './entry';
 import * as _ from 'lodash';
+import { inject } from '@iamthes/inject';
+const FileSystem = fs;
 
 const checkExtensions = ['', '.ts', '.d.ts', '.js', '.tsx', '.jsx'];
 
@@ -19,7 +21,7 @@ export function findFile(name, dirname = '.'): Promise<string> {
         file = testFile;
         break;
     }
-    return Promise.resolve(file)
+    return Promise.resolve(file);
 }
 
 export function uniqEntryList(entryListCollection) {
@@ -27,4 +29,34 @@ export function uniqEntryList(entryListCollection) {
         .flatten<Entry>()
         .uniqBy(entry => entry.hash())
         .value();
+}
+
+export function fileList(basedir: string, File = inject('fs', () => fs)): Promise<string[]> {
+    let result: string[] = [];
+    return new Promise<string[]>(resolve => {
+        File.readdir(basedir, (err, files) => {
+            // Ignore all errors.
+            if (!(files && files.length > 0)) {
+                return resolve([]);
+            }
+            const promises = [];
+            files.forEach(file => {
+                if (file === '.' || file === '..') return;
+                const testPath = Path.join(basedir, file);
+                File.stat(testPath, (err, stat) => {
+                    if (err) return;
+                    if (stat.isDirectory()) {
+                        let p = fileList(testPath).then(r => result = result.concat(r));
+                        promises.push(p);
+                    } else {
+                        result.push(testPath);
+                    }
+                });
+            });
+            const done = () => resolve(result);
+            Promise.all(promises)
+                .then(done)
+                .catch(done);
+        });
+    });
 }
