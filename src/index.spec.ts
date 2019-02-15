de625b5baba2ae305059664d72ecab130bc09f8e
roman.vasilev <root@localhost>
2019-02-16 00:11:09 +0400

    test: Added more tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -7,8 +7,8 @@ it('smoke test', () => {
 });
 
 it('var export', () => {
-    const [result] = main(`export var test = 1`);
-    assert(result.name === 'test');
+    const result = main(`export var test = 1`);
+    assert(result.find(x => x.name === 'test'));
 });
 
 it('declare module', () => {
@@ -46,17 +46,167 @@ it('export interface', () => {
     assert(result.find(x => x.name === 'Entry'));
 });
 
-// it('export default var', () => {
-//     const code = `export default component`;
-//     const [entry] = main(code);
-//     assert(entry);
-//     assert.equal(entry.name, 'component');
-//     assert.equal(entry.isDefault, true);
+it('export default var', () => {
+    const result = main(`export default component`);
+    assert(result.find(x => x.name === 'component' && x.isDefault === true));
+});
+
+it('export default', () => {
+    const result = main(`export default function foo() {}`);
+    assert(result.find(x => x.name === 'foo' && x.isDefault === true));
+});
+
+it('export declare class', () => {
+    const result = main(`export declare class Calendar`);
+    assert(result.find(x => x.name === 'Calendar'));
+});
+
+it('export all', () => {
+    const result = main(`export * from './provider'`);
+    assert(result.find(x => x.specifier === './provider'));
+});
+
+it('export some from module', () => {
+    const result = main(`export {var1} from './provider'`);
+    assert(result.find(x => x.name === 'var1'));
+});
+
+it('pick export', () => {
+    const result = main(`export { CalendarEvent, EventAction } from 'calendar-utils'`);
+    assert(result.find(x => x.name === 'CalendarEvent'));
+    assert(result.find(x => x.name === 'EventAction'));
+});
+
+it('export class', () => {
+    const result = main(`export class Aaa`);
+    assert(result.find(x => x.name === 'Aaa'));
+});
+
+it('empty source', () => {
+    const result = main('');
+    assert.deepEqual(result, []);
+});
+
+it('object binding', () => {
+    const result = main(`export const {ModuleStore} = $traceurRuntime`);
+    assert(result.find(x => x.name === 'ModuleStore'));
+});
+
+it('should extract declared module http', () => {
+    const result = main(`declare module "http" {
+        export var METHODS: string[];
+        export const c1, c2: any;
+    }
+    `);
+    assert(result.find(x => x.module === 'http' && x.name === 'METHODS'));
+    assert(result.find(x => x.module === 'http' && x.name === 'c1'));
+    assert(result.find(x => x.module === 'http' && x.name === 'c2'));
+});
+
+it('should extract declared module events', () => {
+    const result = main(`declare module "events" {
+        class internal extends NodeJS.EventEmitter { }
+        namespace internal {
+            export class EventEmitter extends internal {
+            }
+        }
+        export = internal;
+    }`);
+    assert(result.find(x => x.name === 'EventEmitter' && x.module === 'events'));
+});
+
+it('not too deep parse', () => {
+    const result = main(`
+    export function deep() {
+        const x = {a: {c: {d: 1}}};
+        return () => a => b => c => () => ({
+            export = x;
+        });
+    };
+}`);
+    assert.equal(result.length, 1);
+    assert(result.find(x => x.name === 'deep'));
+});
+
+// it('duplicates must be removed', () => {
+//     const source = `
+//     export function spawnSync(command: string): SpawnSyncReturns<Buffer>;
+//     export function spawnSync(command: string, options?: SpawnSyncOptionsWithStringEncoding): SpawnSyncReturns<string>;
+// `;
+//     const result = main(source);
+//     assert.equal(result.length, 1);
+//     assert.equal(result[0].name, 'spawnSync');
 // });
 
-// it('export default', () => {
-//     const code = `export default function foo() {}`;
-//     const [entry] = parse(code);
-//     assert.equal(entry.name, 'foo');
-//     assert.equal(entry.isDefault, true);
+// it('simulated commonjs', () => {
+//     const source = `
+// declare namespace e {
+//     interface Request extends core.Request { }
+// }
+// declare namespace d {
+//     declare const x = 1;
+//     function y() {}
+// }
+// export = e;
+// `;
+//     const result = main(source, { module: 'somecjs' });
+//     assert.equal(result[0].name, 'Request');
+//     assert.equal(result[0].cjs, true);
+// });
+
+// it('preact declaration', () => {
+//     const source = `
+// declare namespace preact {
+//     function rerender();
+//     type AnyComponent = {};
+// }
+// declare module "preact" {
+//     export = preact;
+// }
+// `;
+//     const result = main(source, { module: 'preact' });
+//     assert(result[0].name === 'rerender');
+//     assert(result[1].name === 'AnyComponent');
+// });
+
+// it('react definitions', () => {
+//     const source = `
+// export = React;
+// export as namespace React;
+// declare namespace React {
+//     type ReactType<P = any> = string | ComponentType<P>;
+//     interface Component<P = {}, S = {}> extends ComponentLifecycle<P, S> { }
+//     class PureComponent<P = {}, S = {}> extends Component<P, S> { }
+// }
+// `;
+//     const result = main(source);
+//     assert(result.length > 0);
+//     assert(result.find(e => e.name === 'ReactType'));
+//     assert(result.find(e => e.name === 'Component'));
+//     assert(result.find(e => e.name === 'PureComponent'));
+// });
+
+// it('webpack', () => {
+//     const source = `
+//         export = webpack;
+//         declare function webpack(options?: webpack.Configuration): webpack.Compiler;
+//         declare namespace webpack {
+//             interface Configuration {
+//             }
+//         }
+//     `;
+//     const result = main(source, { module: 'webpack' });
+//     const configuration: Entry = result.find(e => e.name === 'Configuration');
+//     assert(configuration);
+// });
+
+// it('declare module function', () => {
+//     const source = `
+//     declare module "fs" {
+//         function readFile();
+//     }
+//     `;
+//     const result = main(source, { module: '@types/node' });
+//     assert(result.length === 1);
+//     assert(result.find(e => e.module === 'fs' && e.name === 'readFile'));
 // });
--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,6 @@ export function main(sourceText: string, options: WalkNodeOptions = { result: []
 }
 
 function walkNode(node: ts.Node, options: WalkNodeOptions): any {
-    debugger;
     // console.log("node.kind", node.kind);
     if (node.kind === ts.SyntaxKind.Identifier) {
         const name = (node as ts.Identifier).text;
@@ -24,36 +23,49 @@ function walkNode(node: ts.Node, options: WalkNodeOptions): any {
             ts.SyntaxKind.VariableStatement,
             ts.SyntaxKind.InterfaceDeclaration,
             ts.SyntaxKind.FunctionDeclaration,
-            ts.SyntaxKind.ExportDeclaration
+            ts.SyntaxKind.ExportDeclaration,
+            ts.SyntaxKind.ExportAssignment,
+            ts.SyntaxKind.ClassDeclaration,
         ]);
-        if (statement &&
-            (statement.kind === ts.SyntaxKind.ExportDeclaration
-                || hasExportModifier(statement)
-            )) {
-            options.result.push(new Entry({ ...options, name }));
+        if (statement && (
+            statement.kind === ts.SyntaxKind.ExportDeclaration
+            || statement.kind === ts.SyntaxKind.ExportAssignment
+            || hasExportModifier(statement)
+            || options.module != null
+        )) {
+            const isDefault = statement.kind === ts.SyntaxKind.ExportAssignment
+                || hasDefaultModifier(statement);
+            // const cjs = (statement as any).isExportEquals === true;
+            options.result.push(new Entry({ ...options, name, isDefault }));
         }
     } else if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
-        const children = node.getChildren();
-        const moduleBlock = children.find(c => c.kind === ts.SyntaxKind.ModuleBlock) as ts.Block | undefined;
-        if (!moduleBlock) {
-            return;
+        if (hasDeclareKeyword(node)) {
+            const moduleBlock = node.getChildren().find(c => c.kind === ts.SyntaxKind.ModuleBlock) as ts.Block | undefined;
+            if (!moduleBlock) {
+                return;
+            }
+            const declaredModule = (node as any).name.text;
+            moduleBlock.forEachChild(node => walkNode(node, { ...options, module: declaredModule }));
+        } else if (options.module) {
+            options.result.push(new Entry({ ...options, name: (node as any).name.text }));
         }
-        moduleBlock
-            .statements.filter((st: any) => st.name && ts.isIdentifier(st.name))
-            .map((node: any) => node.name.text)
-            .forEach(name => {
-                options.result.push(new Entry({ ...options, name, module: (node as any).name.text }));
-            });
+    } else if (node.kind === ts.SyntaxKind.ExportDeclaration) {
+        const specifier = ((node as ts.ExportDeclaration).moduleSpecifier as any);
+        if (specifier) {
+            options.result.push(new Entry({ ...options, specifier: specifier.text }));
+        }
     }
-    ts.forEachChild(node, (node) => walkNode(node, options));
+    node.forEachChild(node => walkNode(node, options));
 }
 
 function getParentOf(node: ts.Node, syntaxKinds: ts.SyntaxKind[]) {
+    let depth = 0;
     while (true) {
+        depth += 1;
         if (node && node.parent && syntaxKinds.includes(node.parent.kind)) {
             return node.parent;
         }
-        if (!node.parent) {
+        if (!node.parent || depth >= 5) {
             break;
         }
         node = node.parent;
@@ -64,4 +76,13 @@ function getParentOf(node: ts.Node, syntaxKinds: ts.SyntaxKind[]) {
 function hasExportModifier(node: ts.Node): boolean {
     const result = node.modifiers && node.modifiers.find(m => m.kind === ts.SyntaxKind.ExportKeyword);
     return result !== undefined;
+}
+
+function hasDefaultModifier(node: ts.Node): boolean {
+    const result = node.modifiers && node.modifiers.find(m => m.kind === ts.SyntaxKind.DefaultKeyword);
+    return result !== undefined;
+}
+
+function hasDeclareKeyword(node: ts.Node): boolean {
+    return node.modifiers && node.modifiers.find(m => m.kind === ts.SyntaxKind.DeclareKeyword) !== undefined
 }
